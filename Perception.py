6c7fd17467d8bd27ca358837c8ee1bf9444f34c6
Christian Marinho <88001806+ChrisTheDragon@users.noreply.github.com>
2022-11-03 11:33:45 -0300

    Refatoração do codigo

--- Perception.py
+++ Perception.py
@@ -1,7 +1,4 @@
-input1 = [0, 0, 1]
-input2 = [1, 0, 1]
-input3 = [1, 1, 1]
-input4 = [1, 1, 0]
+inputs = [[0, 0, 1], [1, 0, 1], [1, 1, 1], [1, 1, 0]]
 target = [1, 1, 0, 0]
 
 peso = [0, 0, 0]
@@ -10,7 +7,9 @@ vies = 0
 delta_vies = 0
 lr = 0.1
 voltas = 2
-        
+
+input_fi = []
+y_input = [0, 0, 0, 0]
 
 def neuron(inputx):
     z = 0
@@ -28,64 +27,24 @@ def ativacao(fi):
 
 
 def treinamento(inputx, targetx, y_inputx):
+    global delta_vies, vies
     for i in range(0, len(inputx)):
             delta_peso[i] = lr * (targetx - y_inputx) * inputx[i]
             peso[i] += delta_peso[i]
+    delta_vies = lr * (targetx - y_inputx) * 1
+    vies += delta_vies
 
 
-cont = 0
-while(cont < voltas):
-    
-    input1_fi = neuron(input1)
-    y_input1 = ativacao(input1_fi)
-    
-    if(y_input1 != target[0]):
-        treinamento(input1, target[0], y_input1)
+for i in range(voltas):
+    for j in range(4):
+        y_input[j] = ativacao(neuron(inputs[j]))
         
-        delta_vies = lr * (target[0] - y_input1) * 1
-        vies += delta_vies
-    
-    print(peso, vies)
-    print('\n')
-
-    input2_fi = neuron(input2)
-    y_input2 = ativacao(input2_fi)
-    
-    if(y_input2 != target[1]):
-        treinamento(input2, target[1], y_input2)
+        if (y_input[j] != target[j]):
+            treinamento(inputs[j], target[j], y_input[j])
         
-        delta_vies = lr * (target[1] - y_input2) * 1
-        vies += delta_vies
-    
-    print(peso, vies)
-    print('\n')
-    
-    input3_fi = neuron(input3)
-    y_input3 = ativacao(input3_fi)
-    
-    if(y_input3 != target[2]):
-        treinamento(input3, target[2], y_input3)
-        
-        delta_vies = lr * (target[2] - y_input3) * 1
-        vies += delta_vies
-
-    print(peso, vies)
-    print('\n')
-    
-    input4_fi = neuron(input4)
-    y_input4 = ativacao(input4_fi)
-    
-    if(y_input4 != target[3]):
-        treinamento(input4, target[3], y_input4)
-        
-        delta_vies = lr * (target[3] - y_input4) * 1
-        vies += delta_vies
-    
-    print(peso, vies)
-    print('\n')
-    
-    cont += 1
-    
+        print(peso, vies)
+        print('\n')
+    j = 0   
 
 print("\n")
 input_n = [0, 0, 0]
